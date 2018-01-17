ff8468c91bfc026098527839ec2229ad0d82881b
Arun Sood <arsood@gmail.com>
2018-01-17 14:41:21 -0800

    UI setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import UserList from "./UserList";
+
+import "./style.css";
+
+ReactDOM.render(
+  <UserList />,
+  document.getElementById("root")
+);
