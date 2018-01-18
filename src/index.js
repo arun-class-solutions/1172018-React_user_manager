a9b98a7ad1cb67e0b29a03270ebe4540e1b71e80
Arun Sood <arsood@gmail.com>
2018-01-17 16:51:07 -0800

    Routing with first route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, Switch } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
 
 import UserList from "./UserList";
 
 import "./style.css";
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <UserList />,
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/users" component={UserList} />
+    </Switch>
+  </Router>,
   document.getElementById("root")
 );
