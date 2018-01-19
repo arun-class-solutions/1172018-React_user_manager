14aba2fb9c7656685a1027c43f574da3b46d4ff5
Arun Sood <arsood@gmail.com>
2018-01-18 16:58:10 -0800

    Modular Axios

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+import backend from "../config/backend";
 
 export const getUsers = () => {
   return (dispatch) => {
     // Inside the "thunk"
     // Make an AJAX GET request to MyAPI endpoint
-    axios
-    .get("http://myapi-profstream.herokuapp.com/api/2aebd5/persons")
+    backend
+    .get("/")
     .then((response) => {
       // Set the array from the API to the state so we can iterate over the users and display them on the UI
       dispatch({
---
+++ src/config/backend.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://myapi-profstream.herokuapp.com/api/2aebd5/persons"
+});
